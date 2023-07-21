401c08a88a6be74760fb8d8d9ad6dc6944228624
Mikhail Azarov <azarov.m.a@yandex.ru>
2023-07-21 14:37:48 +0300

    Последние наработки перед рефакторингом кода и наведением порядка

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -6,10 +6,12 @@ from django.contrib import admin
 from django.urls import include, path
 
 
+
+
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
  # В конце добавляем к списку вызов функции static.
- 
+
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,25 +1,33 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
+# from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 from .models import Birthday
 
-def edit_birthday(request, pk):
-    # Находим запрошенный объект для редактирования по первичному ключу
-    # или возвращаем 404 ошибку, если такого объекта нет.
-    instance = get_object_or_404(Birthday, pk=pk)
-    # Связываем форму с найденным объектом: передаём его в аргумент instance.
-    form = BirthdayForm(request.POST or None, instance=instance)
-    # Всё остальное без изменений.
-    context = {'form': form}
-    # Сохраняем данные, полученные из формы, и отправляем ответ:
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context)
+# Импортируем класс пагинатора.
+from django.core.paginator import Paginator
+
+# def edit_birthday(request, pk):
+#     # Находим запрошенный объект для редактирования по первичному ключу
+#     # или возвращаем 404 ошибку, если такого объекта нет.
+#     instance = get_object_or_404(Birthday, pk=pk)
+#     # Связываем форму с найденным объектом: передаём его в аргумент instance.
+#     form = BirthdayForm(request.POST or None, instance=instance)
+#     # Всё остальное без изменений.
+#     context = {'form': form}
+#     # Сохраняем данные, полученные из формы, и отправляем ответ:
+#     if form.is_valid():
+#         form.save()
+#         birthday_countdown = calculate_birthday_countdown(
+#             form.cleaned_data['birthday']
+#         )
+#         context.update({'birthday_countdown': birthday_countdown})
+#     return render(request, 'birthday/birthday.html', context)
 
 # Старая функция birthday - имеет много повторяющегося кода с edit_birthday
 # edit_birthday интегрирована в новую функцию birthday
@@ -78,8 +86,88 @@ def delete_birthday(request, pk):
 
 
 def birthday_list(request):
-    # Получаем все объекты модели Birthday из БД.
-    birthdays = Birthday.objects.all()
-    # Передаём их в контекст шаблона.
-    context = {'birthdays': birthdays}
-    return render(request, 'birthday/birthday_list.html', context) 
+    # Получаем список всех объектов с сортировкой по id.
+    birthdays = Birthday.objects.order_by('id')
+    # Создаём объект пагинатора с количеством 10 записей на страницу.
+    paginator = Paginator(birthdays, 10)
+
+    # Получаем из запроса значение параметра page.
+    page_number = request.GET.get('page')
+    # Получаем запрошенную страницу пагинатора. 
+    # Если параметра page нет в запросе или его значение не приводится к числу,
+    # вернётся первая страница.
+    page_obj = paginator.get_page(page_number)
+    # Вместо полного списка объектов передаём в контекст 
+    # объект страницы пагинатора
+    context = {'page_obj': page_obj}
+    return render(request, 'birthday/birthday_list.html', context)
+
+# Наследуем класс от встроенного ListView:
+class BirthdayListView(ListView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # ...сортировку, которая будет применена при выводе списка объектов:
+    ordering = 'id'
+    # ...и даже настройки пагинации:
+    paginate_by = 10 
+
+# class BirthdayCreateView(CreateView):
+#     # Указываем модель, с которой работает CBV...
+#     model = Birthday
+#     # Указываем имя формы:
+#     form_class = BirthdayForm
+#     # Этот класс сам может создать форму на основе модели!
+#     # Нет необходимости отдельно создавать форму через ModelForm.
+#     # Указываем поля, которые должны быть в форме:
+#     # fields = '__all__'
+#     # Явным образом указываем шаблон:
+#     template_name = 'birthday/birthday.html'
+#     # Указываем namespace:name страницы, куда будет перенаправлен пользователь
+#     # после создания объекта:
+#     success_url = reverse_lazy('birthday:list')
+
+# class BirthdayUpdateView(UpdateView):
+#     model = Birthday
+#     form_class = BirthdayForm
+#     template_name = 'birthday/birthday.html'
+#     success_url = reverse_lazy('birthday:list')
+    
+# class BirthdayDeleteView(DeleteView):
+#     model = Birthday
+#     success_url = reverse_lazy('birthday:list') 
+
+class BirthdayMixin:
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+
+
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
+    pass
+
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+    pass
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday 
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday
+        )
+        # Возвращаем словарь контекста.
+        return context
